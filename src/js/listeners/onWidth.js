5eafaffa17fc35dc9ca2f75bf2a5e21ab24c7935
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2022-02-16 00:00:06 +0200

    fix_bag_window_size_width

--- src/js/listeners/onWidth.js
+++ src/js/listeners/onWidth.js
@@ -1,11 +1,11 @@
 import { refs } from '../base/refs';
 
 export default function onWidth(e) {
-  //   console.log(e.target.outerWidth);
+  // console.log(e.target.outerWidth);
   //   console.log(refs.nameMain);
   //   console.log(refs.nameSidebar);
 
-  if (e.target.outerWidth > 767) {
+  if (e.target.outerWidth >= 768) {
     refs.nameMain.classList.remove('hidden');
     refs.nameSidebar.classList.add('hidden');
     return;
